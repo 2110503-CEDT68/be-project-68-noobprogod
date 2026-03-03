7ac310b59cd8f5bab97bd540e9df290810d88218
Nutthajet <tin.nutthajet@gmail.com>
2026-03-03 15:45:30 +0700

    Correct Spelling

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,13 +41,13 @@ exports.login = async (req,res,next) => {
         const user = await User.findOne({email}).select('+password');
         if(!user){
             return res.status(400).json({success:false,
-                msg:'Invalid credentails'
+                msg:'Invalid credentials'
             });
         }
         const isMatch = await user.matchPassword(password);
         if(!isMatch){
             return res.status(401).json({success:false, 
-                msg:'Invalid credentails'
+                msg:'Invalid credentials'
             });
         }
         // const token = user.getSignedJwtToken();
